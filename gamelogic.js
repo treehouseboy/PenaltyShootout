f63ef2e5eaf5661e5470dbfa6778de37c6fa20f0
Daniel Smith <daniel.smith@academytrainee.com>
2018-06-15 10:00:10 +0100

    player and pitch rendering

--- gamelogic.js
+++ gamelogic.js
@@ -1,3 +1,43 @@
+var gameCanvas;
+var GameCanvasContext;
+
+var playerX;
+var playerY;
+var playerRadius;
+var playerSpeed;
+
+var goalKeeperX;
+var goalKeeperY;
+var goalKeeperSpeed;
+
+var ballX;
+var ballY;
+var ballSpeed;
+
+
 function gameInit() {
-  console.log("hello");
+  gameCanvas = document.getElementById("gameCanvas");
+  gameCanvas.style.backgroundColor = "#09b800";
+  GameCanvasContext = gameCanvas.getContext("2d");
+  playerX = gameCanvas.width/2;
+  playerY = gameCanvas.height - 50;
+  playerRadius = 15;
+  setInterval(gameRun,16);
+}
+
+function gameRun() {
+
+  render();
+}
+
+function render() {
+  renderPlayer();
+}
+
+function renderPlayer() {
+  GameCanvasContext.beginPath();
+  GameCanvasContext.arc(playerX, playerY, playerRadius, 0, Math.PI*5);
+  GameCanvasContext.fillStyle = "#f200ff";
+  GameCanvasContext.fill();
+  GameCanvasContext.closePath();
 }
